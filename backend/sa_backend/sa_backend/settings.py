9717464ec0669cd07f4d5d418b0ef069137582dd
illia@ITIllushka <276487@student.pwr.edu.pl>
2024-11-11 10:57:02 +0100

    Database credentials added

--- backend/sa_backend/sa_backend/settings.py
+++ backend/sa_backend/sa_backend/settings.py
@@ -74,10 +74,14 @@ WSGI_APPLICATION = 'sa_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.postgresql',
+'NAME': 'sa_db', # change to your database name
+'USER': 'DBAdmin', # change to your user name
+'PASSWORD': 'SpeechAdmin2024', # change to your user password
+'HOST': 'speech-assistant-db.c3ye6syo2fhj.eu-central-1.rds.amazonaws.com',
+'PORT': '5432', # change only if you changed the default port
+}
 }
 
 
