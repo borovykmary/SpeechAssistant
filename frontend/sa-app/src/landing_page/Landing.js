6f744992da66253e0d62a9b976dbc92f89d346e7
Nikola Różycka <276075@student.pwr.edu.pl>
2024-11-12 20:25:00 +0100

    Front end for landing page

---
+++ frontend/sa-app/src/landing_page/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Landing.css';
+import logo from '../assets/logo.png'; 
+
+function Landing() {
+    return (
+        <div className="landing-page">
+            <header className="header">
+                <img src={logo} alt="Speech Assistant Logo" className="logo" />
+                <h1>Introducing</h1>
+                <h2 className="app-name">Speech Assistant</h2>
+                <p className="description">
+                    “An application designed to help users who struggle with public presentations
+                    improve their pronunciation, emotional expression, and manage performance anxiety
+                    before and after speaking events.”
+                </p>
+                <button className="get-started">Get Started</button>
+            </header>
+            <div className="illustration-container">
+                <img src="../assets/illustration.png" alt="Illustration" className="illustration" />
+            </div>
+        </div>
+    );
+}
+
+export default Landing;
