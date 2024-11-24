2491eb394874fd082d1d12c35237bbb8823b7b58
maryna <276069@student.pwr.edu.pl>
2024-11-24 18:31:29 +0100

    Audio input saving implemented, user_id is  mocked

--- backend/sa_backend/sa_app/urls.py
+++ backend/sa_backend/sa_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', views.login_user, name='login_user'),
     path('logout/', views.logout_user, name='logout_user'),
     path('register/', views.register_user, name='register_user'),
-    path('', views.landing_page, name='landing_page')
+    path('', views.landing_page, name='landing_page'),
+    path('upload_audio/', views.upload_audio, name='upload_audio')
 ]
