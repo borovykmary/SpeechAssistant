67553f304b34a0bc8050606b0d17fd96d51f1178
Oliwia <oliwiaskrgit@gmail.com>
2024-12-06 18:55:00 +0100

    feature_authorization_backend- handling authorization in the backend, view to check whether someone is logged in to communicate with frontend

--- backend/sa_backend/sa_app/urls.py
+++ backend/sa_backend/sa_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', views.login_user, name='login_user'),
     path('logout/', views.logout_user, name='logout_user'),
     path('register/', views.register_user, name='register_user'),
-    path('', views.landing_page, name='landing_page')
+    path('', views.landing_page, name='landing_page'),
+    path('check_login/', views.check_login, name='check_login'),
 ]
